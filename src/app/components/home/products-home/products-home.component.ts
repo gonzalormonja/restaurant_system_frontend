fedb61316640b31ea901308f34e08600b44ee50d
Gonzalo Monja <gonzalormonja1@gmail.com>
2022-02-28 16:59:42 -0300

    agrego buscador de productos recientes

--- src/app/components/home/products-home/products-home.component.ts
+++ src/app/components/home/products-home/products-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Menu } from 'src/app/interfaces/menu';
 import { MenuService } from 'src/app/services/menu/menu.service';
 
@@ -11,10 +12,30 @@ export class ProductsHomeComponent implements OnInit {
   constructor(private menuService: MenuService) {}
 
   products: Menu[] = [];
+  products_backup: Menu[] = [];
+  search = new FormControl('');
 
   ngOnInit(): void {
     this.menuService.get('', 'createdAt', 'desc', 0, 10).subscribe((resp) => {
       this.products = resp.data;
+      this.products_backup = resp.data;
+    });
+
+    this.search.valueChanges.subscribe((val) => {
+      if (val.trim().length <= 0) {
+        this.products = this.products_backup;
+      }
+      val = new RegExp(val, 'i');
+      this.products = this.products_backup.filter((product) => {
+        if (
+          product.name.match(val) ||
+          product.category.name.match(val) ||
+          product.price.toString().match(val)
+        ) {
+          return true;
+        }
+        return false;
+      });
     });
   }
 }
