49ac44abc1a57cfd775fc19f87c33cbfe5117081
Gonzalo Monja <gonzalormonja1@gmail.com>
2022-03-11 07:10:51 -0300

    agrego busqueda en home hacia api no local

--- src/app/components/home/products-home/products-home.component.ts
+++ src/app/components/home/products-home/products-home.component.ts
@@ -12,30 +12,37 @@ export class ProductsHomeComponent implements OnInit {
   constructor(private menuService: MenuService) {}
 
   products: Menu[] = [];
-  products_backup: Menu[] = [];
+  // products_backup: Menu[] = [];
   search = new FormControl('');
 
   ngOnInit(): void {
     this.menuService.get('', 'createdAt', 'desc', 0, 10).subscribe((resp) => {
       this.products = resp.data;
-      this.products_backup = resp.data;
+      // this.products_backup = resp.data;
     });
 
     this.search.valueChanges.subscribe((val) => {
-      if (val.trim().length <= 0) {
-        this.products = this.products_backup;
-      }
-      val = new RegExp(val, 'i');
-      this.products = this.products_backup.filter((product) => {
-        if (
-          product.name.match(val) ||
-          product.category.name.match(val) ||
-          product.price.toString().match(val)
-        ) {
-          return true;
-        }
-        return false;
-      });
+      this.menuService
+        .get(val, 'createdAt', 'desc', 0, 10)
+        .subscribe((resp) => {
+          this.products = resp.data;
+          // this.products_backup = resp.data;
+        });
+
+      // if (val.trim().length <= 0) {
+      //   this.products = this.products_backup;
+      // }
+      // val = new RegExp(val, 'i');
+      // this.products = this.products_backup.filter((product) => {
+      //   if (
+      //     product.name.match(val) ||
+      //     product.category.name.match(val) ||
+      //     product.price.toString().match(val)
+      //   ) {
+      //     return true;
+      //   }
+      //   return false;
+      // });
     });
   }
 }
